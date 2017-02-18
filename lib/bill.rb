17a7602a667b37618616cbbee8a89174c6c372f2
Liam Taylor <liam.taylor888@googlemail.com>
2017-02-18 14:30:22 +0000

    implements get_bill method to make API call and parse response

--- lib/bill.rb
+++ lib/bill.rb
@@ -4,6 +4,13 @@ class Bill
 
   def initialize(bill_url)
     @bill_url = bill_url
+
+  end
+
+  def get_bill
+    uri = URI(@bill_url)
+    response = Net::HTTP.get(uri)
+    result = JSON.parse(response)
   end
 
 end

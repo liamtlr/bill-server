d54b50a772c3dc91ba1b94a1d2f3203eb44b01c1
Liam Taylor <liam.taylor888@googlemail.com>
2017-02-18 13:21:46 +0000

    creates failing test for rendering of response to API call for bill contents

--- spec/features/bill_spec.rb
+++ spec/features/bill_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 feature 'Viewing a bill' do
-  scenario 'display bill contents' do
+  it 'displays an introductory phrase to the bill' do
     visit('/')
-    expect(page).to have_content 'Here is your bill for February'
+    expect(page).to have_content 'Here is your bill for February:'
   end
+  it 'displays the result of an API call containing the bill details' do
+    uri = URI('http://safe-plains-5453.herokuapp.com/bill.json')
+    response = Net::HTTP.get(uri)
+    visit('/')
+    expect(page).to have_content response
+  end
+
+
 end

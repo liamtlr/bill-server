5b2ff37c1baa344d5bdf3dedcb49894d706cc954
Liam Taylor <liam.taylor888@googlemail.com>
2017-02-18 14:29:00 +0000

    creates failing test for bill object getting bill contest from API

--- spec/bill_spec.rb
+++ spec/bill_spec.rb
@@ -4,8 +4,17 @@ describe Bill do
   let(:subject) { described_class.new(bill_url) }
 
   describe '#initialize' do
-    it 'receives a URL upon intiialization' do
+    it 'receives a URL upon intialization' do
       expect(subject.bill_url).to eq bill_url
+    end
+  end
+
+  describe '#get_bill' do
+    it 'makes an API call to the URL to get the bill contents' do
+      uri = URI('http://safe-plains-5453.herokuapp.com/bill.json')
+      response = Net::HTTP.get(uri)
+      result = JSON.parse(response)
+      expect(subject.get_bill).to eq result
     end
   end
 end
